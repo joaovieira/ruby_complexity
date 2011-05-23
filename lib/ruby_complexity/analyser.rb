39d3e17aa514addb9bdd9452b8beb0731cf33716
Joao Vieira <shocker@abandonados.info>
2011-05-23 02:07:24 +0100

    Removido layout e head/body do html de detalhe gerado.

--- lib/ruby_complexity/analyser.rb
+++ lib/ruby_complexity/analyser.rb
@@ -8,7 +8,7 @@ module RubyComplexity
       MetricFu::Configuration.run do |config|
         config.base_directory = File.join(results_dir, 'metrics')
 	    config.data_directory = File.join(config.base_directory, '_data')
-	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M")}", RubyComplexity::ANALYSER.underscore)	
+	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M%S")}", RubyComplexity::ANALYSER.underscore)	
   	    config.template_class = RcomplexityTemplate	
 	    config.metrics = [:flog]
   	    config.graphs = [:flog]
@@ -19,7 +19,7 @@ module RubyComplexity
       # Run metric fu!
       MetricFu.metrics.each {|metric| MetricFu.report.add(metric) }
       MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.base_directory, "report.yml")
-      MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.data_directory, "#{date.strftime("%Y%m%d%H%M")}.yml")
+      MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.data_directory, "#{date.strftime("%Y%m%d%H%M%S")}.yml")
 
       MetricFu.graphs.each {|graph| MetricFu.graph.add(graph, MetricFu.graph_engine) }
       MetricFu.graph.generate
