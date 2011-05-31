e6132dd21e36d204134200fcf8e5d5af55520316
Joao Vieira <shocker@abandonados.info>
2011-05-31 20:01:54 +0100

    Modificado stylesheet da visao detalhada

--- lib/ruby_complexity/analyser.rb
+++ lib/ruby_complexity/analyser.rb
@@ -8,8 +8,8 @@ module RubyComplexity
       MetricFu::Configuration.run do |config|
         config.base_directory = File.join(results_dir, 'metrics')
 	    config.data_directory = File.join(config.base_directory, '_data')
-	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M%S")}", RubyComplexity::ANALYSER.underscore)	
-  	    config.template_class = RcomplexityTemplate	
+	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M%S")}", RubyComplexity::ANALYSER.underscore)
+	    config.template_class = RcomplexityTemplate	
 	    config.metrics = [:flog]
   	    config.graphs = [:flog]
 	    config.code_dirs = [File.join(results_dir,'app'), File.join(results_dir,'lib')]
